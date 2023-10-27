16a42f18c3f7a2173f9a71d6da0be049ef297470
Saptarsi <saptarsismmitra@gmail.com>
2023-10-27 17:42:26 +0530

    updated..

--- src/Route/Navbar.js
+++ src/Route/Navbar.js
@@ -9,20 +9,20 @@ import { NavLink } from 'react-router-dom'
 
 export default function Navbar() {
     let handleNav = (()=>{
-        // let nav_Btn = document.querySelector('#navBtn')
         let nav_Menu = document.querySelector('.nav')
         nav_Menu.classList.toggle('hide')
+        nav_Menu.classList.remove('active')
     })
     return (
         <>
         <button id='navBtn' onClick={handleNav}><i className="fa-solid fa-bars"></i></button>
-        <div className='nav hide'>
-            <h2 className='links'><NavLink to='/'>Home</NavLink></h2>
-            <h2 className='links'><NavLink to='/Bollywood'>Bollywood</NavLink></h2>
-            <h2 className='links'><NavLink to='/Technology'>Technology</NavLink></h2>
-            <h2 className='links'><NavLink to='/Food'>Food</NavLink></h2>
-            <h2 className='links'><NavLink to='Hollywood'>Hollywood</NavLink></h2>
-            <h2 className='links'><NavLink to='/Fitness'>Fitness</NavLink></h2>
+        <div className='nav hide active'>
+            <h2 className='links'><NavLink onClick={handleNav} className='active' to='/'>Home</NavLink></h2>
+            <h2 className='links'><NavLink onClick={handleNav} className='active' to='/Bollywood'>Bollywood</NavLink></h2>
+            <h2 className='links'><NavLink onClick={handleNav} className='active' to='/Technology'>Technology</NavLink></h2>
+            <h2 className='links'><NavLink onClick={handleNav} className='active' to='/Food'>Food</NavLink></h2>
+            <h2 className='links'><NavLink onClick={handleNav} className='active' to='Hollywood'>Hollywood</NavLink></h2>
+            <h2 className='links'><NavLink onClick={handleNav} className='active' to='/Fitness'>Fitness</NavLink></h2>
         </div>
         </>
     )
